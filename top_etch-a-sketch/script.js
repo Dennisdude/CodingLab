1cb964d41220d88779d79a11132ff546e53c6c29
Dennis Deifel <dennisdeifel@gmail.com>
2025-10-30 01:26:03 +0100

    Add grid size adjustment functionality

--- top_etch-a-sketch/script.js
+++ top_etch-a-sketch/script.js
@@ -1,6 +1,12 @@
 let gridSize = 16;
 
-const canvasContainer = document.querySelector('#canvas');
+const canvasContainer = document.querySelector('#canvasContainer');
+const gridSizeText = document.querySelector('#gridSizeText');
+const gridSizeSlider = document.querySelector('#gridSizeSlider')
+
+function clearGrid() {
+  canvasContainer.innerHTML = '';
+}
 
 function buildGrid() {
   for (let i = 0; i < (gridSize*gridSize); i++) {
@@ -11,6 +17,14 @@ function buildGrid() {
     canvasSquare.style.width = 100/gridSize + '%';
   }
 }
+
+gridSizeSlider.addEventListener('input', e => {
+  clearGrid();
+  
+  gridSize = e.target.value;
+  gridSizeText.textContent = `${gridSize}x${gridSize}`;
+  buildGrid();
+});
 
 function init() {
   buildGrid();
