5872abd647cd3ba8e661dd1022ec82fbfde9d0b6
Dennis Deifel <dennisdeifel@gmail.com>
2025-10-30 17:22:09 +0100

    Refactor controls layout and add grid border option

--- top_etch-a-sketch/script.js
+++ top_etch-a-sketch/script.js
@@ -1,6 +1,7 @@
 let gridSize = 16;
 
 const canvasContainer = document.querySelector('#canvasContainer');
+const gridBordersCheckbox = document.querySelector('#gridBordersCheckbox');
 const gridSizeText = document.querySelector('#gridSizeText');
 const gridSizeSlider = document.querySelector('#gridSizeSlider')
 
@@ -9,6 +10,8 @@ function clearGrid() {
 }
 
 function buildGrid() {
+  gridSizeText.textContent = `${gridSize}x${gridSize}`;
+
   for (let i = 0; i < (gridSize*gridSize); i++) {
     const canvasSquare = document.createElement('div');
     canvasContainer.appendChild(canvasSquare);
@@ -16,21 +19,39 @@ function buildGrid() {
     canvasSquare.style.height = 100/gridSize + '%';
     canvasSquare.style.width = 100/gridSize + '%';
   }
+
+  gridBordersCheckbox.dispatchEvent(new CustomEvent('change'));
 }
 
 canvasContainer.addEventListener('mouseover', e => {
   e.target.style.backgroundColor = "red";
 });
 
+gridBordersCheckbox.addEventListener('change', () => {
+  const canvasSquares = document.querySelectorAll('.square');
+
+  if (gridBordersCheckbox.checked) {
+    canvasSquares.forEach(square => {
+      square.classList.add('squareBorder');
+    });
+  } else {
+    canvasSquares.forEach(square => {
+      square.classList.remove('squareBorder');
+    });
+  }
+});
+
 gridSizeSlider.addEventListener('input', e => {
-  clearGrid();
   gridSize = e.target.value;
+  
+  clearGrid();
   buildGrid();
-  gridSizeText.textContent = `${gridSize}x${gridSize}`;
 });
 
 function init() {
   buildGrid();
+
+  gridBordersCheckbox.dispatchEvent(new CustomEvent('change'));
 }
 
 init();
